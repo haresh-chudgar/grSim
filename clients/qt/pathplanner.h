4dac792ef99238bfd259fd896ea9962f8a53c1e4
Samer Nashed <snashed@cs.umass.edu>
2016-04-28 15:35:22 -0400

    RRT path planning. Has NOT been tested. Has NOT been integrated.

---
+++ clients/qt/pathplanner.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 <copyright holder> <email>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef PATHPLANNER_H
+#define PATHPLANNER_H
+
+#include <eigen3/Eigen/Core>
+#include <vector>
+#include "soccerfieldinfo.h"
+
+class PathPlanner {
+  public:
+    PathPlanner(int robot_ID, bool team);
+    ~PathPlanner();
+   
+    std::vector<Eigen::Vector3d> FindPath(Eigen::Vector3d start_pos, Eigen::Vector3d goal_pos);
+
+    Eigen::Vector3d start_pos_;
+    Eigen::Vector3d goal_pos_;
+
+  private:
+    double distToLineSeg(Eigen::Vector2d v, Eigen::Vector2d w, Eigen::Vector2d p);
+    bool checkCollisions(Eigen::Vector3d v, Eigen::Vector3d w, vector<Eigen::Vector2d> dyn_obj_loc);
+    pair<RRTNode, RRTNode> NearestNodes(vector<RRTNode> FRRT, vector<RRTNode> BRRT);
+    vector<Eigen::Vector3d> FindWaypoints(vector<RRTNode> FRRT, vector<RRTNode> BRRT, RRTNode F_terminal, RRTNode B_terminal);
+    vector<Eigen::Vector3d> SmoothWaypoints(vector<Eigen::Vector3d> waypoints, vector<Eigen::Vector2d> dyn_obj_loc);
+    double fRand(double fMin, double fMax);
+
+    bool team_;
+    int robot_ID_;
+    static const double x_lim_ = 3.0; //mm
+    static const double y_lim_ = 2.0; //mm
+    static const int node_lim_ = 5000;
+    static const double destination_epsilon_ = 0.005; //m
+    static const double goal_bias_ = 0.15;
+    static const double robot_radius_ = 0.08; //m
+};
+
+#endif
